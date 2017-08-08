1abd1e045dd25c43f8a8262fa7ce4ce99d7b9b70
Cesar Tessarin <cesar.tessarin@gmail.com>
2017-08-08 16:22:24 -0300

    Basic options processing.

--- src/main.c
+++ src/main.c
@@ -9,20 +9,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <DTImage.h>
 #include <DTDither.h>
+#include <DTPalette.h>
 
 int
 main(int argc, char ** argv)
 {
-    if (argc != 3) {
-	fprintf(stderr, "Usage: %s input output", argv[0]);
+    char *paletteID = NULL;
+    char *inputFile, *outputFile;
+    int c;
+
+    opterr = 0;
+
+    while ((c = getopt(argc, argv, "p:")) != -1) {
+	switch (c) {
+	    case 'p':
+		paletteID = optarg;
+		break;
+	    case '?':
+		fprintf(stderr, "Ignoring unknown option: -%c\n", optopt);
+	}
+    }
+
+    // check if there is still two arguments remaining, for i/o
+    if (argc - optind != 2) {
+	fprintf(stderr, "Usage: %s [-p palette] input output\n", argv[0]);
 	exit(EXIT_FAILURE);
     }
 
-    DTImage *input = CreateImageFromFile(argv[1]);
-    ApplyFloydSteinbergDither(input);
-    WriteImageToFile(input, argv[2]);
+    // all arguments ok
+    inputFile = argv[optind];
+    outputFile = argv[optind+1];
+
+    DTImage *input = CreateImageFromFile(inputFile);
+    if (input == NULL) return 1;
+
+    ApplyFloydSteinbergDither(input, StandardPaletteRGB());
+    WriteImageToFile(input, outputFile);
 
     return 0;
 }
